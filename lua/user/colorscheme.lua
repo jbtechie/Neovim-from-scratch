a85894c0e2c36a05916e2ff67c264d9f028082c2
Justin Bailey <jbtechie@gmail.com>
2022-08-20 11:48:26 -0500

    sync active/inactive pane colors across nvim and tmux

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -5,4 +5,9 @@ catch /^Vim\%((\a\+)\)\=:E185/
   colorscheme default
   set background=dark
 endtry
+
+" let Tmux control bg color for active window
+hi Normal guibg=none
+" use same inactive color as Tmux
+hi NormalNC guibg=#4e4e4e
 ]]
